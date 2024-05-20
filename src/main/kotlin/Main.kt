8f8e14cb1a6ba6cb023f2f54b56c5a1ef81be275
Jovan Cvijanović <119356876+AStroCvijo@users.noreply.github.com>
2024-05-20 03:09:16 +0200

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,11 @@ import kotlin.system.exitProcess
 
 // Define the TokenInfo class
 data class TokenInfo(
-    val t: String, // token
+    val t: String,  // token
     val fp: String, // path
-    val p: Int,    // position
-    val h: String  //hash
+    val p: Int,     // position
+    val h: String,  // hash
+    val l: Int      // line number
 )
 
 // HashMap to store hashed tokens
@@ -24,7 +25,6 @@ fun insertToken(tokenInfo: TokenInfo) {
 
 // Main function
 fun main(args: Array<String>) {
-
     println("RUN!")
 
     // No arguments provided
@@ -40,23 +40,23 @@ fun main(args: Array<String>) {
     var case = false
 
     // Parse arguments
-    for (i in args.indices){
-        if ((args[i] == "-p" || args[i] == "-path") && i+1<args.size){
-            folderPath = args[i+1]
+    for (i in args.indices) {
+        if ((args[i] == "-p" || args[i] == "-path") && i + 1 < args.size) {
+            folderPath = args[i + 1]
         }
-        if(args[i] == "-f" || args[i] == "-force"){
+        if (args[i] == "-f" || args[i] == "-force") {
             force = true
         }
-        if(args[i] == "-q" || args[i] == "-query"){
+        if (args[i] == "-q" || args[i] == "-query") {
             query = true
         }
-        if(args[i] == "-c" || args[i] == "-case"){
+        if (args[i] == "-c" || args[i] == "-case") {
             case = true
         }
     }
 
     // Execution logic based on parsed arguments
-    if(folderPath != ""){
+    if (folderPath != "") {
         // File object representing the folder
         val folder = File(folderPath)
 
@@ -68,22 +68,20 @@ fun main(args: Array<String>) {
 
         // File path for the tokenMap.json
         var tokenMapFilePath = ".\\indexedFolders\\" + folderPath + "_tokenMap.json"
-        if(case){
+        if (case) {
             tokenMapFilePath = ".\\indexedFolders\\" + folderPath + "CASE_tokenMap.json"
         }
 
         // Logic if force = false
-        if(!force){
+        if (!force) {
             // Check if they already exist
             if (File(tokenMapFilePath).exists()) {
-
-                if(query){
+                if (query) {
                     tokenMap = loadTokenMapFromJson(tokenMapFilePath)
                     if (tokenMap.isNotEmpty()) {
                         println("tokenMap loaded from $tokenMapFilePath successfully.")
                     }
-                }
-                else{
+                } else {
                     println("tokenMap for $tokenMapFilePath already exists.")
                 }
 
@@ -96,8 +94,8 @@ fun main(args: Array<String>) {
                 saveTokenMapToJson(tokenMapFilePath)
             }
 
-            // Starts prompting the user for query's
-            if(query){
+            // Starts prompting the user for queries
+            if (query) {
                 // Loop until Enter is pressed
                 var searchString: String
                 while (true) {
@@ -120,7 +118,7 @@ fun main(args: Array<String>) {
 
                     // Retrieve token information from the HashMap
                     var tokenInfoList = tokenMap[searchString.lowercase().hashCode().toString()]
-                    if(case){
+                    if (case) {
                         tokenInfoList = tokenMap[searchString.hashCode().toString()]
                     }
 
@@ -129,7 +127,7 @@ fun main(args: Array<String>) {
                         if (tokenInfoList.isNotEmpty()) {
                             println("Search Results:")
                             tokenInfoList.forEach { tokenInfo ->
-                                println("String '$searchString' found in file '${tokenInfo.fp}' at position ${tokenInfo.p}")
+                                println("String '$searchString' found in file '${tokenInfo.fp}' in line ${tokenInfo.l}, position in file ${tokenInfo.p}")
                             }
                             println("Found ${tokenInfoList.size} occurrences in the indexed folder")
                         }
@@ -137,12 +135,10 @@ fun main(args: Array<String>) {
                         println("No matches found for '$searchString'.")
                     }
                 }
-            }
-            else{
+            } else {
                 return
             }
-        }
-        else{
+        } else {
             // Loops over the folder and its subfolders and tokenizes the words in files
             val allowedExtensions = readAllowedExtensionsFromFile()
             loopFilesAndSubfolders(folder, allowedExtensions, case)
@@ -150,8 +146,8 @@ fun main(args: Array<String>) {
             // Save tokenMap to JSON file
             saveTokenMapToJson(tokenMapFilePath)
 
-            // Starts prompting the user for query's
-            if(query){
+            // Starts prompting the user for queries
+            if (query) {
                 // Loop until Enter is pressed
                 var searchString: String
                 while (true) {
@@ -174,7 +170,7 @@ fun main(args: Array<String>) {
 
                     // Retrieve token information from the HashMap
                     var tokenInfoList = tokenMap[searchString.lowercase().hashCode().toString()]
-                    if(case){
+                    if (case) {
                         tokenInfoList = tokenMap[searchString.hashCode().toString()]
                     }
 
@@ -183,7 +179,7 @@ fun main(args: Array<String>) {
                         if (tokenInfoList.isNotEmpty()) {
                             println("Search Results:")
                             tokenInfoList.forEach { tokenInfo ->
-                                println("String '$searchString' found in file '${tokenInfo.fp}' at position ${tokenInfo.p}")
+                                println("String '$searchString' found in file '${tokenInfo.fp}' in line ${tokenInfo.l}, position in file ${tokenInfo.p}")
                             }
                             println("Found ${tokenInfoList.size} occurrences in the indexed folder")
                         }
@@ -191,13 +187,12 @@ fun main(args: Array<String>) {
                         println("No matches found for '$searchString'.")
                     }
                 }
-            }
-            else{
+            } else {
                 return
             }
         }
-    }
-    else{
+    } else {
         println("No folder path argument!")
     }
 }
+
